ea5e1f9c0d6b5cda2eb2ef76f7925b5b80531a12
Vinayyad9897 <vinayyadav91190@gmail.com>
2024-05-29 00:10:55 +0530

    add common template in base.html file

--- src/main/java/com/scm/scm/controller/PageController.java
+++ src/main/java/com/scm/scm/controller/PageController.java
@@ -3,10 +3,9 @@ package com.scm.scm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class MyController {
+public class PageController {
 
     @GetMapping("/home")
     public String home(Model model) {
@@ -19,10 +18,18 @@ public class MyController {
         model.addAttribute("title", "Hello World");
         model.addAttribute("links", "Hello World");
         return "base";
-    } @GetMapping("/service")
+    }
+    @GetMapping("/service")
     public String service(Model model) {
         model.addAttribute("title", "Hello World");
         model.addAttribute("links", "Hello World");
         return "service";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("isLogin", false);
+
+        return "about";
     }
 }

b73d6d4c465fc7e59bec8cd3b5a8f2bf043d13c0
Vinay Kumar Yadav <91029277+Vinay9897@users.noreply.github.com>
2024-09-05 17:04:23 +0530

    Update PageController.java

--- src/main/java/com/scm/scm/controller/PageController.java
+++ src/main/java/com/scm/scm/controller/PageController.java
@@ -8,7 +8,7 @@ import com.scm.scm.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model; 
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
